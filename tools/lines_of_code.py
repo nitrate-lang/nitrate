9ffc10a0ce2dd3267af105cf848eea7c884796ba
Wesley Jones <wesjones2004@gmail.com>
2024-11-05 12:54:55 -0500

    fix sloc script

--- tools/lines_of_code.py
+++ tools/lines_of_code.py
@@ -3,72 +3,85 @@
 # Count lines of code in this repo
 
 import os
+from sys import exit
+from os.path import join, islink
+from typing import Tuple, List
 
-valid_extensions = [".c", ".h", ".cpp", ".cc", ".hpp", ".py", ".q", ".qh", ".java", ".js", ".ts", ".html", ".tsx", ".jsx", ".json",
-                    ".xml", ".yml", ".yaml", ".sh", ".bat", ".cmd", ".ps1", ".css", ".scss", ".sass", ".php", ".go", ".rs", ".cs", ".swift", ".m", ".mm", "go"]
-exclude_dirs = ['./docs', './build', './.git', 'node_modules', 'test', "deps"]
+valid_extensions = [".c", ".h", ".cpp", ".cc", ".hpp", ".py", ".q", ".qh"]
+exclude_dirs = ['./build', './.build', './.git', 'third_party', 'binary_data']
 exclude_files = ["package-lock.json"]
 
 
-def get_loc(path) -> (int, int):
+def get_sloc(path) -> Tuple[List, int, int]:
     """
-    Returns the number of lines of code in the given path.
+    Returns the number of source lines of code in the given path recursively.
     """
-    loc = 0
-    files_count = 0
-    file_loc = []
-    for root, dirs, files in os.walk(path):
+
+    def is_source_line(line) -> bool:
+        comment_prefixes = ["//", "/*", "*",
+                            "#", "*/", "'''", '"""', '--', '~>']
+
+        line = line.decode().strip()
+        if not line:
+            return False
+
+        if any([line.startswith(prefix) for prefix in comment_prefixes]):
+            return False
+
+        return True
+
+    total_sloc = 0
+
+    file_sloc_map = []
+    for dirpath, _, filenames in os.walk(path):
         # Check if the current directory should be excluded by prefix
-        skip = False
-        for exclude_dir in exclude_dirs:
-            if root.startswith(exclude_dir) or exclude_dir in root:
-                skip = True
-                break
-        if skip:
+        if any([dirpath.startswith(exclude_dir) or exclude_dir in dirpath for exclude_dir in exclude_dirs]):
             continue
 
-        for file in files:
-            if file in exclude_files:
-                continue
-            if not file.endswith(tuple(valid_extensions)):
+        for file in filenames:
+            # Check if the file should be excluded by name or extension
+            if file in exclude_files or not file.endswith(tuple(valid_extensions)):
                 continue
 
             # Check if is symlink
-            if os.path.islink(os.path.join(root, file)):
+            if islink(join(dirpath, file)):
                 continue
+
             try:
-                with open(os.path.join(root, file), "r") as f:
-                    files_count += 1
-                    tmp = loc
+                with open(join(dirpath, file), "rb") as f:
+                    sloc_in_file = 0
                     for line in f:
-                        tline = line.strip()
-                        # Check if line is a comment or empty
-                        if tline == "":
-                            continue
-                        loc += 1
-                    file_loc.append((os.path.join(root, file), loc - tmp))
+                        if is_source_line(line):
+                            sloc_in_file += 1
+
+                    file_sloc_map.append(
+                        (join(dirpath, file), sloc_in_file))
+                    total_sloc += sloc_in_file
             except UnicodeDecodeError:
-                print(f"UnicodeDecodeError: {os.path.join(root, file)}")
-                continue
+                print(f"UnicodeDecodeError: {join(dirpath, file)}")
+                exit(1)
 
     # Sort by lines of code
-    file_loc.sort(key=lambda x: x[1], reverse=False)
-    for file in file_loc:
-        print(f"\x1b[36;49m{file[0]}\x1b[0m: {file[1]}")
+    file_sloc_map.sort(key=lambda x: x[1], reverse=False)
 
-    return loc, files_count
+    return file_sloc_map, total_sloc
 
 
 def main():
-    loc, count = get_loc(".")
-    loc = str(loc)
-    # Format with commas
-    loc = loc[::-1]
-    loc = ",".join(loc[i:i+3] for i in range(0, len(loc), 3))
-    loc = loc[::-1]
+    sloc_map, sloc = get_sloc(".")
+
+    for file in sloc_map:
+        print(f"\x1b[36;49m{file[0]}\x1b[0m: {file[1]}")
+
+    # Format number
+    sloc = str(sloc)
+    sloc = sloc[::-1]
+    sloc = ",".join(sloc[i:i+3] for i in range(0, len(sloc), 3))
+    sloc = sloc[::-1]
 
     print()
-    print(f"Lines of code: {loc} ({count} code files)")
+    print(f"\x1b[34;49;1mLines of code:\x1b[0m {
+          sloc} ({len((sloc_map))} code files)")
 
 
 if __name__ == "__main__":
