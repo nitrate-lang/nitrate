5b8c7481580a5feeeaffd3b25089ef52bd0c8d73
Wesley Jones <wesjones2004@gmail.com>
2024-12-13 19:18:24 -0500

    work

--- pipeline/libnitrate-parser/src/descent/Function.cc
+++ pipeline/libnitrate-parser/src/descent/Function.cc
@@ -31,50 +31,48 @@
 ///                                                                          ///
 ////////////////////////////////////////////////////////////////////////////////
 
-/// TODO: Source location
-
 /// TODO: Cleanup this code; it's a mess from refactoring.
 
 #include <descent/Recurse.hh>
 
 using namespace npar;
 
-static std::optional<FuncParam> recurse_fn_parameter(npar_t &S, qlex_t &rd) {
-  auto tok = next();
-
-  std::string name;
-  Type *type = nullptr;
-
-  if (!tok.is(qName)) {
-    diagnostic << tok << "Expected a parameter name before ':'";
-  }
-
-  name = tok.as_string(&rd);
-  tok = peek();
-
-  if (tok.is<qPuncColn>()) {
-    next();
-
-    type = recurse_type(S, rd);
-
-    tok = peek();
+static Type *recurse_function_parameter_type(npar_t &S, qlex_t &rd) {
+  if (next_if(qPuncColn)) {
+    return recurse_type(S, rd);
   } else {
-    type = make<InferTy>();
+    let type = make<InferTy>();
+    type->set_offset(current().start);
+    return type;
   }
+}
 
-  if (tok.is<qOpSet>()) {
-    next();
-    tok = peek();
-
-    Expr *value =
-        recurse_expr(S, rd,
-                     {qlex_tok_t(qPunc, qPuncComa),
-                      qlex_tok_t(qPunc, qPuncRPar), qlex_tok_t(qOper, qOpGT)});
-
-    return FuncParam(SaveString(name), type, value);
+static std::optional<Expr *> recurse_function_parameter_value(npar_t &S,
+                                                              qlex_t &rd) {
+  if (next_if(qOpSet)) {
+    return recurse_expr(
+        S, rd,
+        {qlex_tok_t(qPunc, qPuncComa), qlex_tok_t(qPunc, qPuncRPar),
+         qlex_tok_t(qOper, qOpGT)});
   } else {
-    return FuncParam(SaveString(name), type, nullptr);
+    return std::nullopt;
   }
+}
+
+static std::optional<FuncParam> recurse_function_parameter(npar_t &S,
+                                                           qlex_t &rd) {
+  if (let name = next_if(qName)) {
+    let param_name = name->as_string(&rd);
+    let param_type = recurse_function_parameter_type(S, rd);
+    let param_value = recurse_function_parameter_value(S, rd);
+
+    return FuncParam{SaveString(param_name), param_type,
+                     param_value.value_or(nullptr)};
+  } else {
+    diagnostic << current() << "Expected a parameter name before ':'";
+  }
+
+  return std::nullopt;
 }
 
 std::optional<TemplateParameters> recurse_template_parameters(npar_t &S,
@@ -95,7 +93,7 @@ std::optional<TemplateParameters> recurse_template_parameters(npar_t &S,
       break;
     }
 
-    if (let param_opt = recurse_fn_parameter(S, rd)) {
+    if (let param_opt = recurse_function_parameter(S, rd)) {
       let param = *param_opt;
       let tparam = TemplateParameter{std::get<0>(param), std::get<1>(param),
                                      std::get<2>(param)};
