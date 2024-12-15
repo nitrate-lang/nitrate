a570607d501c6ca42c30cda0e9e14e37da49d77c
Wesley Jones <wesjones2004@gmail.com>
2024-12-14 23:59:07 -0500

    Pipeline API usage examples

---
+++ api-examples/source-to-tokens.cc
@@ -0,0 +1,20 @@
+#include <nitrate/code.hh>
+
+int main() {
+  const std::string source = R"(
+    pub fn main(): i32 {
+      ret 42;
+    }
+  )";
+
+  std::string tokens_json;
+  if (auto future = nitrate::pipeline(source, tokens_json, {"lex"}).get()) {
+    std::cout << tokens_json;
+
+    return 0;
+  } else {
+    std::cerr << "Compilation failed" << std::endl;
+
+    return 1;
+  }
+}
