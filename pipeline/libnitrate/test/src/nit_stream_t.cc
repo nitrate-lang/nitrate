8fa7cbebeae198b670db59c3f931e93de974a812
Wesley Jones <wesjones2004@gmail.com>
2024-12-14 20:44:06 -0500

    Add some tests for the stream code

--- pipeline/libnitrate/test/src/nit_stream_t.cc
+++ pipeline/libnitrate/test/src/nit_stream_t.cc
@@ -3,16 +3,63 @@
 #include <cstdio>
 #include <nitrate/code.hh>
 
-TEST(nit_stream_t, OpenClose) {
-  std::string content = "This is some example content";
-  FILE* content_file = fmemopen((void*)content.c_str(), content.size(), "r");
-
-  nit_stream_t* ss = nit_from(content_file, false);
-  if (ss == nullptr) { /* 'nit_from' returns nullptr on failure */
-    if (content_file) fclose(content_file);
-    SUCCEED();
+class CFile {
+public:
+  CFile(FILE* file) : file_(file) {}
+  CFile(CFile&& other) : file_(other.file_) { other.file_ = nullptr; }
+  ~CFile() {
+    if (file_) {
+      fclose(file_);
+    }
   }
 
-  nit_fclose(ss);
-  fclose(content_file);
+  FILE* get() { return file_; }
+
+  operator FILE*() { return file_; }
+
+private:
+  FILE* file_;
+};
+
+TEST(nit_stream_t, EchoContent) {
+  const std::string text = "This is some example content";
+  auto in_file = CFile(fmemopen((void*)text.c_str(), text.size(), "r"));
+  ASSERT_NE(in_file.get(), nullptr);
+
+  auto out_file = CFile(tmpfile());
+
+  EXPECT_TRUE(nitrate::pipeline(in_file.get(), out_file.get(), {"echo"}).get());
+  rewind(out_file);
+
+  char buffer[sizeof(text)];
+  auto n = fread(buffer, 1, sizeof(buffer), out_file);
+  ASSERT_EQ(n, text.size());
+  ASSERT_EQ(text, std::string(buffer, n));
+}
+
+TEST(nit_stream_t, ConcatStream) {
+  const std::vector<std::string> texts = {"TextA", "TextB", "TextC", "TextD",
+                                          "TextE"};
+  std::string expected_text;
+
+  std::vector<CFile> in_files;
+  std::vector<FILE*> in_files_ptrs;
+  for (const auto& text : texts) {
+    auto file = fmemopen((void*)text.c_str(), text.size(), "r");
+    ASSERT_NE(file, nullptr);
+    in_files.push_back(CFile(file));
+    in_files_ptrs.push_back(file);
+
+    expected_text += text;
+  }
+
+  auto out_file = CFile(tmpfile());
+  EXPECT_TRUE(nitrate::pipeline(in_files_ptrs, out_file.get(), {"echo"}).get());
+
+  rewind(out_file);
+
+  char buffer[1024];
+  auto n = fread(buffer, 1, sizeof(buffer), out_file);
+  ASSERT_EQ(n, expected_text.size());
+  ASSERT_EQ(expected_text, std::string(buffer, n));
 }
