6cb6147021e315cfc20fef54474e93524cd8073e
Wesley Jones <wesjones2004@gmail.com>
2024-12-31 14:19:10 -0500

    fix

--- tools/quix-lex.cc
+++ tools/quix-lex.cc
@@ -1,7 +1,7 @@
 #include <nitrate/code.hh>
 
 int main() {
-  auto future = nitrate::pipeline(stdin, stdout, {"lex"});
+  auto future = nitrate::pipeline(std::cin, std::cout, {"lex"});
 
   return future.get() ? 0 : 1;
 }
