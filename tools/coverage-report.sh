368c10b37cb2d7e5c7e4c1335d3dd55082768e7b
Wesley Jones <wesjones2004@gmail.com>
2025-02-16 17:12:49 -0500

    Coverage Gutters Integration

--- tools/coverage-report.sh
+++ tools/coverage-report.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
+EXECUTABLES=`find ./build/bin -type f -executable`
+SHARED_LIBS=`find ./build/lib -type f -name "*.so"`
+OBJECTS="$EXECUTABLES $SHARED_LIBS"
+
 if [ -z "$LLVM_PROFILE_FILE" ]; then
     LLVM_PROFILE_FILE=default.profraw
 fi
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <path-to-binary>"
+llvm-profdata-18 merge -sparse $LLVM_PROFILE_FILE -o default.profdata
+if [ $? -ne 0 ]; then
+    echo "Failed to merge profile data"
+    rm -f default.profdata
     exit 1
 fi
 
-PATH_TO_OBJECT=$1
+mkdir -p coverage
 
-llvm-profdata-18 merge -sparse $LLVM_PROFILE_FILE -o default.profdata || exit 1
-llvm-cov-18 report $PATH_TO_OBJECT -instr-profile=default.profdata --use-color || (rm -f default.profdata && exit 1)
+for OBJECT in $OBJECTS; do
+  FILENAME=`basename $OBJECT`
+  llvm-cov-18 export $OBJECT --format=lcov -instr-profile=default.profdata > coverage/$FILENAME.dat
+  if [ $? -ne 0 ]; then
+    echo "Failed to generate coverage report"
+    rm -f default.profdata
+    exit 1
+  fi
+done
 
 rm -f default.profdata
+
+echo "Complete"
