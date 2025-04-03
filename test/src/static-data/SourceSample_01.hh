41bc6d7140b6a6a7583748507ddbc569ab2fb48c
Wesley Jones <wesjones2004@gmail.com>
2025-04-02 20:31:02 -0400

    fix

--- test/src/static-data/SourceSample_01.hh
+++ test/src/static-data/SourceSample_01.hh
@@ -56,9 +56,6 @@ struct [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
   pub fn foo();
   pro fn bar();
   sec fn baz();
-  pub static fn foo();
-  pro static fn bar();
-  sec static fn baz();
 }
 
 region [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
@@ -71,9 +68,6 @@ region [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
   pub fn foo();
   pro fn bar();
   sec fn baz();
-  pub static fn foo();
-  pro static fn bar();
-  sec static fn baz();
 }
 
 class [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
@@ -86,9 +80,6 @@ class [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
   pub fn foo();
   pro fn bar();
   sec fn baz();
-  pub static fn foo();
-  pro static fn bar();
-  sec static fn baz();
 }
 
 group [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
@@ -101,9 +92,6 @@ group [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
   pub fn foo();
   pro fn bar();
   sec fn baz();
-  pub static fn foo();
-  pro static fn bar();
-  sec static fn baz();
 }
 
 union [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
@@ -116,9 +104,6 @@ union [Special, 10] MyStruct<x: i32 = 0, i = 0, t>: Object, MyTrait {
   pub fn foo();
   pro fn bar();
   sec fn baz();
-  pub static fn foo();
-  pro static fn bar();
-  sec static fn baz();
 }
 
 enum hello: u32 {
