ba04ed893d4a451d5fb9feed289839e1b2f89dea
Wesley Jones <wesjones2004@gmail.com>
2025-04-05 00:06:01 -0400

    work

--- no3/lib/src/core/package/Package.hh
+++ no3/lib/src/core/package/Package.hh
@@ -83,7 +83,7 @@ namespace no3::package {
     [[nodiscard]] auto Validate() const -> bool;
 
     ///=============================================================================================
-    ///==                                   WRITING FUNCTIONS                                     ==
+    ///==                                    STORE FUNCTIONS                                      ==
     enum class GenericStoreKind {
       Folder,
       TextFile,
@@ -101,10 +101,25 @@ namespace no3::package {
      *       It is the responsibility of the caller to manage the stream.
      * @note No references to any shared streams will be retained after the Store() call returns.
      */
-    using GenericStore = std::function<bool(GenericStoreKind kind, const std::filesystem::path& relative_path,
-                                            std::shared_ptr<std::ostream>& writer)>;
+    using GenericStoreOperation = std::function<bool(GenericStoreKind kind, const std::filesystem::path& relative_path,
+                                                     std::shared_ptr<std::ostream>& writer)>;
 
-    [[nodiscard]] auto Store(const GenericStore& dest) -> bool;
+    /**
+     * @brief Store the package.
+     * @param dest Implementation of the GenericStoreOperation interface.
+     * @param write_content Should the content of emission files will be written to their stream.
+     * @return true if no failures occurred during the store operation. These may include
+     *         GenericStoreOperation returning false, the stream write failing, or
+     *         GenericStoreOperation(File|BinaryFile) returning true, but the output parameter
+     *         being nullptr. Otherwise, false.
+     * @note In the event of any failure, the Store() operation may cancel prematurely, leaving
+     *       partially written package in an unspecified state.
+     */
+    [[nodiscard]] auto Store(const GenericStoreOperation& dest, bool write_content = true) -> bool;
     [[nodiscard]] auto LocalStore(const std::filesystem::path& base_path) const -> bool;
+
+    ///=============================================================================================
+    ///==                                    DEBUG FUNCTIONS                                      ==
+    [[nodiscard]] auto SummarizeTree(std::ostream& os) const -> bool;
   };
 }  // namespace no3::package
