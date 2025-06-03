7769e920d71436353ecb8b0b7758ed9bd1af29b5
Wesley Jones <wesjones2004@gmail.com>
2025-06-03 17:20:15 -0400

    keywords

--- compiler/lexer/include/nitrate-lexer/Keyword.hh
+++ compiler/lexer/include/nitrate-lexer/Keyword.hh
@@ -24,17 +24,21 @@
 
 namespace nitrate::compiler::lexer {
   enum class Keyword : uint8_t {
-    /* Code Organization */
-    Scope,  /* 'scope' */
-    Import, /* 'import' */
-    Pub,    /* 'pub' */
-    Sec,    /* 'sec' */
-    Pro,    /* 'pro' */
-
-    /* Code Symbols */
-    Let, /* 'let' */
-    Var, /* 'var' */
-    Fn,  /* 'fn' */
+    /* Storage */
+    Let,      /* 'let' */
+    Var,      /* 'var' */
+    Fn,       /* 'fn' */
+    Enum,     /* 'enum' */
+    Struct,   /* 'struct' */
+    Class,    /* 'class' */
+    Union,    /* 'union' */
+    Contract, /* 'interface' */
+    Trait,    /* 'trait' */
+    Type,     /* 'type' */
+    Opaque,   /* 'opaque' */
+    Scope,    /* 'scope' */
+    Import,   /* 'import' */
+    UnitTest, /* 'unit_test' */
 
     /* Modifiers */
     Safe,    /* 'safe' */
@@ -43,6 +47,9 @@ namespace nitrate::compiler::lexer {
     Static,  /* 'static' */
     Mut,     /* 'mut' */
     Const,   /* 'const' */
+    Pub,     /* 'pub' */
+    Sec,     /* 'sec' */
+    Pro,     /* 'pro' */
 
     /* Control Flow */
     If,       /* 'if' */
@@ -66,31 +73,26 @@ namespace nitrate::compiler::lexer {
     Null,  /* 'null' */
     True,  /* 'true' */
     False, /* 'false' */
-
-    /* Data Types */
-    Type,     /* 'type' */
-    Opaque,   /* 'opaque' */
-    Enum,     /* 'enum' */
-    Struct,   /* 'struct' */
-    Class,    /* 'class' */
-    Contract, /* 'interface' */
-    Trait,    /* 'trait' */
-    Union,    /* 'union' */
   };
 
   static inline const boost::bimap<Keyword, std::string_view> KEYWORD_MAP = [] {
     boost::bimap<Keyword, std::string_view> mapping;
     auto& map = mapping.left;
 
-    map.insert({Keyword::Scope, "scope"});
-    map.insert({Keyword::Import, "import"});
-    map.insert({Keyword::Pub, "pub"});
-    map.insert({Keyword::Sec, "sec"});
-    map.insert({Keyword::Pro, "pro"});
-
     map.insert({Keyword::Let, "let"});
     map.insert({Keyword::Var, "var"});
     map.insert({Keyword::Fn, "fn"});
+    map.insert({Keyword::Enum, "enum"});
+    map.insert({Keyword::Struct, "struct"});
+    map.insert({Keyword::Class, "class"});
+    map.insert({Keyword::Union, "union"});
+    map.insert({Keyword::Contract, "interface"});
+    map.insert({Keyword::Trait, "trait"});
+    map.insert({Keyword::Opaque, "opaque"});
+    map.insert({Keyword::Type, "type"});
+    map.insert({Keyword::Scope, "scope"});
+    map.insert({Keyword::Import, "import"});
+    map.insert({Keyword::UnitTest, "unit_test"});
 
     map.insert({Keyword::Safe, "safe"});
     map.insert({Keyword::Unsafe, "unsafe"});
@@ -98,6 +100,9 @@ namespace nitrate::compiler::lexer {
     map.insert({Keyword::Static, "static"});
     map.insert({Keyword::Mut, "mut"});
     map.insert({Keyword::Const, "const"});
+    map.insert({Keyword::Pub, "pub"});
+    map.insert({Keyword::Sec, "sec"});
+    map.insert({Keyword::Pro, "pro"});
 
     map.insert({Keyword::If, "if"});
     map.insert({Keyword::Else, "else"});
@@ -119,15 +124,6 @@ namespace nitrate::compiler::lexer {
     map.insert({Keyword::Null, "null"});
     map.insert({Keyword::True, "true"});
     map.insert({Keyword::False, "false"});
-
-    map.insert({Keyword::Type, "type"});
-    map.insert({Keyword::Opaque, "opaque"});
-    map.insert({Keyword::Enum, "enum"});
-    map.insert({Keyword::Struct, "struct"});
-    map.insert({Keyword::Class, "class"});
-    map.insert({Keyword::Contract, "interface"});
-    map.insert({Keyword::Trait, "trait"});
-    map.insert({Keyword::Union, "union"});
 
     return mapping;
   }();
