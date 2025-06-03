472bda0528e26e7484d2fa791ae0ec51738ea53a
Wesley Jones <wesjones2004@gmail.com>
2025-06-02 21:22:35 -0400

    Keywords

--- compiler/lexer/include/nitrate-lexer/Keyword.hh
+++ compiler/lexer/include/nitrate-lexer/Keyword.hh
@@ -19,6 +19,62 @@
 
 #pragma once
 
+#include <boost/flyweight.hpp>
+#include <cstdint>
+
 namespace nitrate::compiler::lexer {
-  // TODO: Write code
-}
+  enum class Keyword : uint8_t {
+    /* Code Organization */
+    Scope,  /* 'scope' */
+    Import, /* 'import' */
+    Pub,    /* 'pub' */
+    Sec,    /* 'sec' */
+    Pro,    /* 'pro' */
+
+    /* Code Symbols */
+    Let, /* 'let' */
+    Var, /* 'var' */
+    Fn,  /* 'fn' */
+
+    /* Modifiers */
+    Safe,    /* 'safe' */
+    Unsafe,  /* 'unsafe' */
+    Promise, /* 'promise' */
+    Static,  /* 'static' */
+    Mut,     /* 'mut' */
+    Const,   /* 'const' */
+
+    /* Control Flow */
+    If,       /* 'if' */
+    Else,     /* 'else' */
+    For,      /* 'for' */
+    While,    /* 'while' */
+    Do,       /* 'do' */
+    Switch,   /* 'switch' */
+    Break,    /* 'break' */
+    Continue, /* 'continue' */
+    Return,   /* 'ret' */
+    Foreach,  /* 'foreach' */
+    Try,      /* 'try' */
+    Catch,    /* 'catch' */
+    Throw,    /* 'throw' */
+    Async,    /* 'async' */
+    Await,    /* 'await' */
+    Asm,      /* 'asm' */
+
+    /* Literals */
+    Null,  /* 'null' */
+    True,  /* 'true' */
+    False, /* 'false' */
+
+    /* Data Types */
+    Type,     /* 'type' */
+    Opaque,   /* 'opaque' */
+    Enum,     /* 'enum' */
+    Struct,   /* 'struct' */
+    Class,    /* 'class' */
+    Contract, /* 'interface' */
+    Trait,    /* 'trait' */
+    Union,    /* 'union' */
+  };
+}  // namespace nitrate::compiler::lexer
