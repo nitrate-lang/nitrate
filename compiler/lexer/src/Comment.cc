2c7723e744bf93839d0ae0fd2f377b546552c0f3
Wesley Jones <wesjones2004@gmail.com>
2025-06-02 23:06:18 -0400

    placeholder implementations

--- compiler/lexer/src/Comment.cc
+++ compiler/lexer/src/Comment.cc
@@ -16,3 +16,18 @@
 ///   * Copyright (C) 2024 Wesley C. Jones                                   ///
 ///                                                                          ///
 ////////////////////////////////////////////////////////////////////////////////
+
+#include <nitrate-lexer/Comment.hh>
+
+using namespace nitrate::compiler::lexer;
+
+BOOST_SYMBOL_EXPORT auto Comment::is_documentation() const -> bool {
+  // TODO: Implement logic to determine if the comment is documentation
+  return false;  // Placeholder implementation
+}
+
+BOOST_SYMBOL_EXPORT auto Comment::is_tool_signal() const -> bool {
+  // TODO: Implement logic to determine if the comment is a signal
+  // to language tools
+  return false;  // Placeholder implementation
+}
