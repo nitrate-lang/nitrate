6c7c913455ba6bb6b241b2036151afdabc3bc1b1
Wesley Jones <wesjones2004@gmail.com>
2025-06-02 23:06:07 -0400

    lexer interface

--- compiler/lexer/include/nitrate-lexer/Lexer.hh
+++ compiler/lexer/include/nitrate-lexer/Lexer.hh
@@ -19,6 +19,34 @@
 
 #pragma once
 
+#include <deque>
+#include <istream>
+#include <nitrate-lexer/Token.hh>
+#include <stack>
+
 namespace nitrate::compiler::lexer {
-  // TODO: Write code
-}
+  class Lexer {
+    std::istream& m_input_stream;
+    std::deque<Token> m_tokens;
+    std::stack<uint32_t> m_rewind_checkpoints;
+    bool m_eof_reached = false;
+
+    [[nodiscard]] auto peek_byte() -> uint8_t;
+    [[nodiscard]] auto next_byte() -> uint8_t;
+
+  public:
+    Lexer(std::istream& is);
+    Lexer(const Lexer&) = delete;
+    Lexer(Lexer&&) = delete;
+    auto operator=(const Lexer&) -> Lexer& = delete;
+    auto operator=(Lexer&&) -> Lexer& = delete;
+    ~Lexer() = default;
+
+    [[nodiscard]] auto next_token() -> Token;
+    [[nodiscard]] auto peek_token(uint8_t k = 1) -> const Token&;
+    [[nodiscard]] auto is_end_of_file() const -> bool { return m_eof_reached && m_tokens.empty(); }
+
+    auto push_rewind_checkpoint() -> void;
+    auto pop_rewind_checkpoint() -> void;
+  };
+}  // namespace nitrate::compiler::lexer
