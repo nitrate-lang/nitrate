c2312b4220ac655b8e822014988b7124bc6d39b6
Wesley Jones <wesjones2004@gmail.com>
2025-06-02 20:58:34 -0400

    Data-structure to model a code comment

--- compiler/lexer/include/nitrate-lexer/Comment.hh
+++ compiler/lexer/include/nitrate-lexer/Comment.hh
@@ -19,6 +19,27 @@
 
 #pragma once
 
+#include <boost/flyweight.hpp>
+#include <cstdint>
+
 namespace nitrate::compiler::lexer {
-  // TODO: Write code
-}
+  enum class CommentType : uint8_t {
+    SingleLine,
+    MultiLine,
+  };
+
+  class Comment {
+    boost::flyweight<std::string> m_content;
+    CommentType m_type;
+
+  public:
+    Comment(CommentType type, boost::flyweight<std::string> content) : m_content(std::move(content)), m_type(type) {}
+
+    [[nodiscard]] constexpr auto type() const -> CommentType { return m_type; }
+    [[nodiscard]] constexpr auto content() const -> const boost::flyweight<std::string>& { return m_content; }
+    [[nodiscard]] constexpr auto is_single_line() const -> bool { return m_type == CommentType::SingleLine; }
+    [[nodiscard]] constexpr auto is_multi_line() const -> bool { return m_type == CommentType::MultiLine; }
+    [[nodiscard]] constexpr auto is_documentation() const -> bool;
+    [[nodiscard]] constexpr auto is_tool_signal() const -> bool;
+  };
+}  // namespace nitrate::compiler::lexer
