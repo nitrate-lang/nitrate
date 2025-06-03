3d09e92d3fd2ac898a1abf2d10e713ae338af120
Wesley Jones <wesjones2004@gmail.com>
2025-06-02 21:00:44 -0400

    update

--- compiler/lexer/include/nitrate-lexer/Comment.hh
+++ compiler/lexer/include/nitrate-lexer/Comment.hh
@@ -36,10 +36,14 @@ namespace nitrate::compiler::lexer {
     Comment(CommentType type, boost::flyweight<std::string> content) : m_content(std::move(content)), m_type(type) {}
 
     [[nodiscard]] constexpr auto type() const -> CommentType { return m_type; }
-    [[nodiscard]] constexpr auto content() const -> const boost::flyweight<std::string>& { return m_content; }
+    [[nodiscard]] constexpr auto content() const -> const std::string& { return m_content.get(); }
+
     [[nodiscard]] constexpr auto is_single_line() const -> bool { return m_type == CommentType::SingleLine; }
     [[nodiscard]] constexpr auto is_multi_line() const -> bool { return m_type == CommentType::MultiLine; }
     [[nodiscard]] auto is_documentation() const -> bool;
     [[nodiscard]] auto is_tool_signal() const -> bool;
+
+    [[nodiscard]] constexpr auto begin() const { return m_content->cbegin(); }
+    [[nodiscard]] constexpr auto end() const { return m_content->cend(); }
   };
 }  // namespace nitrate::compiler::lexer
