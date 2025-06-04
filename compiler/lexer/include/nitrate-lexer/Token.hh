15d4aaf338f950eadb6ec1dbd3bfece2c771cf8d
Wesley Jones <wesjones2004@gmail.com>
2025-06-04 12:35:59 -0400

    add hash functions

--- compiler/lexer/include/nitrate-lexer/Token.hh
+++ compiler/lexer/include/nitrate-lexer/Token.hh
@@ -51,6 +51,8 @@ namespace nitrate::compiler::lexer {
     FileSourceLocation(uint32_t line, uint32_t column, uint32_t offset)
         : m_line(line), m_column(column), m_offset(offset) {}
 
+    [[nodiscard]] constexpr auto operator<=>(const FileSourceLocation& other) const -> std::strong_ordering = default;
+
     [[nodiscard]] auto line() const -> uint32_t { return m_line; }
     [[nodiscard]] auto column() const -> uint32_t { return m_column; }
     [[nodiscard]] auto offset() const -> uint32_t { return m_offset; }
@@ -66,10 +68,17 @@ namespace nitrate::compiler::lexer {
     FileSourceRange(boost::flyweight<std::string> file, FileSourceLocation begin, FileSourceLocation end)
         : m_file(std::move(file)), m_begin(begin), m_end(end) {}
 
+    [[nodiscard]] auto operator<=>(const FileSourceRange& other) const -> std::strong_ordering = default;
+
     [[nodiscard]] auto file() const -> const std::string& { return m_file.get(); }
     [[nodiscard]] auto begin() const -> FileSourceLocation { return m_begin; }
     [[nodiscard]] auto end() const -> FileSourceLocation { return m_end; }
   };
+
+  inline auto hash_value(const FileSourceRange& range) -> size_t {
+    return std::hash<std::string>()(range.file()) ^ std::hash<uint32_t>()(range.begin().offset()) ^
+           std::hash<uint32_t>()(range.end().offset());
+  }
 
   class Token {
   public:
