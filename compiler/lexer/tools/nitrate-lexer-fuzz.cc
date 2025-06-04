450d34eb5665d8c340eea34e9d94a9069df1d863
Wesley Jones <wesjones2004@gmail.com>
2025-06-03 22:01:55 -0400

    updates

---
+++ compiler/lexer/tools/nitrate-lexer-fuzz.cc
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+///                                                                          ///
+///     .-----------------.    .----------------.     .----------------.     ///
+///    | .--------------. |   | .--------------. |   | .--------------. |    ///
+///    | | ____  _____  | |   | |     ____     | |   | |    ______    | |    ///
+///    | ||_   _|_   _| | |   | |   .'    `.   | |   | |   / ____ `.  | |    ///
+///    | |  |   \ | |   | |   | |  /  .--.  \  | |   | |   `'  __) |  | |    ///
+///    | |  | |\ \| |   | |   | |  | |    | |  | |   | |   _  |__ '.  | |    ///
+///    | | _| |_\   |_  | |   | |  \  `--'  /  | |   | |  | \____) |  | |    ///
+///    | ||_____|\____| | |   | |   `.____.'   | |   | |   \______.'  | |    ///
+///    | |              | |   | |              | |   | |              | |    ///
+///    | '--------------' |   | '--------------' |   | '--------------' |    ///
+///     '----------------'     '----------------'     '----------------'     ///
+///                                                                          ///
+///   * NITRATE TOOLCHAIN - The official toolchain for the Nitrate language. ///
+///   * Copyright (C) 2024 Wesley C. Jones                                   ///
+///                                                                          ///
+////////////////////////////////////////////////////////////////////////////////
+
+#define FMT_HEADER_ONLY
+#include <spdlog/spdlog.h>
+
+#include <nitrate-lexer/Lexer.hh>
+
+using namespace nitrate::compiler::lexer;
+
+extern "C" auto LLVMFuzzerTestOneInput(const uint8_t *Data,  // NOLINT(readability-identifier-naming)
+                                       size_t Size) -> int {
+  (void)Data;  // Suppress unused parameter warning
+  (void)Size;  // Suppress unused parameter warning
+
+  return 0;  // Values other than 0 and -1 are reserved for future use.
+}
