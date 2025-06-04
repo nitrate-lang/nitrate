f8559bfc1061003260c8ca15877cb5273d73b716
Wesley Jones <wesjones2004@gmail.com>
2025-06-03 22:06:46 -0400

    Create lexer fuzzer

--- compiler/lexer/tools/nitrate-lexer-fuzz.cc
+++ compiler/lexer/tools/nitrate-lexer-fuzz.cc
@@ -17,17 +17,35 @@
 ///                                                                          ///
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <boost/flyweight/flyweight_fwd.hpp>
 #define FMT_HEADER_ONLY
 #include <spdlog/spdlog.h>
 
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/stream.hpp>
 #include <nitrate-lexer/Lexer.hh>
 
 using namespace nitrate::compiler::lexer;
 
-extern "C" auto LLVMFuzzerTestOneInput(const uint8_t *Data,  // NOLINT(readability-identifier-naming)
-                                       size_t Size) -> int {
-  (void)Data;  // Suppress unused parameter warning
-  (void)Size;  // Suppress unused parameter warning
+static const auto LEXER_FILENAME = boost::flyweight<std::string>("stdin");
+
+extern "C" auto LLVMFuzzerInitialize(int *, char ***) -> int {  // NOLINT(readability-identifier-naming)
+  // This function is called once at the start of the fuzzer.
+  // It can be used to initialize global state, set up logging, etc.
+  // Here we disable logging for the fuzzer.
+
+  spdlog::set_level(spdlog::level::off);
+
+  return 0;  // Return 0 to indicate successful initialization.
+}
+
+extern "C" auto LLVMFuzzerTestOneInput(const uint8_t *data,  // NOLINT(readability-identifier-naming)
+                                       size_t size) -> int {
+  auto input = boost::iostreams::stream<boost::iostreams::array_source>(reinterpret_cast<const char *>(data), size);
+  auto lexer = Lexer(input, LEXER_FILENAME);
+
+  while (lexer.next_token()) {
+  }
 
   return 0;  // Values other than 0 and -1 are reserved for future use.
 }
