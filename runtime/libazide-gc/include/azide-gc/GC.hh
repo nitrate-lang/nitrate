41d6a241072a4357fb8201d0ed6900ca98317568
Wesley Jones <wesjones2004@gmail.com>
2025-06-08 13:14:15 -0400

    designing GC API

--- runtime/libazide-gc/include/azide-gc/GC.hh
+++ runtime/libazide-gc/include/azide-gc/GC.hh
@@ -1,7 +1,6 @@
 #ifndef __AZIDE_GC_GC_H__
 #define __AZIDE_GC_GC_H__
 
-#include <array>
 #include <cstddef>
 #include <cstdint>
 
@@ -16,19 +15,21 @@ namespace azide::gc {
   struct GC;
 
   struct TaskAPI {
-    using Suspend = void (*)(void* user_data, uint64_t task_id);
-    using Resume = void (*)(void* user_data, uint64_t task_id);
+    using PauseTasks = void (*)(void* m);
+    using ResumeTasks = void (*)(void* m);
 
-    Suspend m_suspend = nullptr;
-    Resume m_resume = nullptr;
-    void* m_user_data = nullptr;
+    PauseTasks m_pause = nullptr;
+    void* m_pause_m = nullptr;
+
+    ResumeTasks m_resume = nullptr;
+    void* m_resume_m = nullptr;
   };
 
   struct AsyncFinalization {
-    using Runner = void (*)(void* user_data, void* base, size_t size, uint64_t object_id);
+    using Runner = void (*)(void* m, void* base, size_t size, uint64_t object_id);
 
     Runner m_runner = nullptr;
-    void* m_user_data = nullptr;
+    void* m_runner_m = nullptr;
   };
 
   /**
@@ -36,6 +37,15 @@ namespace azide::gc {
    *
    * This function allocates and returns a pointer to a new GC (Garbage Collector) object.
    * The returned GC instance is ready for use in memory management operations.
+   *
+   * The GC instance will be in the disabled state upon creation.
+   *
+   * @param task_api A structure containing function pointers for task management.
+   *                 These functions are used to pause and resume tasks during garbage collection.
+   *                 If the `m_pause` or `m_resume` pointers are null, the GC will not pause tasks during collection.
+   * @param runner   A structure containing a function pointer for asynchronous finalization.
+   *                 This function is called to finalize objects that are no longer reachable.
+   *                 If the `m_runner` is null, nullptr is returned from this function.
    *
    * @return GC* Pointer to the newly created garbage collector instance.
    *         Returns nullptr if creation fails.
@@ -110,8 +120,7 @@ namespace azide::gc {
    * The memory range *need not* be valid upon registration, but it must be
    * valid before any calls to `azide_gc_step`.
    *
-   * This operation will complete successfully, regardless of whether the GC is
-   * enabled or disabled.
+   * This operation will complete, regardless of whether the GC is enabled or disabled.
    *
    * This function will ignore any byte within the specified range that is
    * already managed by the GC. If the entire range is already managed, the function
@@ -123,10 +132,11 @@ namespace azide::gc {
    * @param gc   Pointer to the garbage collector instance.
    * @param base Pointer to the start of the memory range to be managed.
    * @param size Size (in bytes) of the memory range to be managed.
+   * @return true if the memory range was successfully registered, false otherwise.
    *
    * @note This function is **thread-safe**, including with respect to the GC instance.
    */
-  extern "C" auto azide_gc_manage(GC* gc, void* base, size_t size) -> void;
+  extern "C" [[nodiscard]] auto azide_gc_manage(GC* gc, void* base, size_t size) -> bool;
 
   /**
    * @brief Removes a memory range from garbage collection management.
@@ -163,45 +173,133 @@ namespace azide::gc {
    */
   extern "C" [[nodiscard]] auto azide_gc_is_managed(GC* gc, void* base, size_t size) -> bool;
 
-  extern "C" auto azide_gc_add_task(GC* gc, uint64_t task_id) -> void;
-  extern "C" auto azide_gc_del_task(GC* gc, uint64_t task_id) -> void;
+  /**
+   * @brief Registers a root pointer with the garbage collector.
+   *
+   * This function adds a pointer to a root variable, preventing the garbage collector
+   * from reclaiming any memory that is reachable from this root. The root should
+   * remain valid for the lifetime of the garbage collector or until it is explicitly
+   * removed.
+   *
+   * @see azide_gc_del_root for removing a root.
+   *
+   * @param gc   Pointer to the garbage collector instance.
+   * @param root Address of the root pointer to be registered.
+   * @return true if the root was successfully added; false otherwise.
+   *
+   * @note This function is **thread-safe**, including with respect to the GC instance.
+   */
+  extern "C" [[nodiscard]] auto azide_gc_add_root(GC* gc, void** root) -> bool;
 
-  extern "C" auto azide_gc_destroyed(GC* gc, uint64_t object_id) -> void;
+  /**
+   * @brief Removes a root pointer from the garbage collector's root set.
+   *
+   * This function unregisters a previously registered root pointer from the
+   * garbage collector, so that the memory it points to is no longer considered
+   * reachable and may be collected if not referenced elsewhere. If the root pointer
+   * is not found in the root set, this function has no effect.
+   *
+   * @param gc   Pointer to the garbage collector instance.
+   * @param root Address of the root pointer to be removed from the root set.
+   *
+   * @note This function is **thread-safe**, including with respect to the GC instance.
+   */
+  extern "C" auto azide_gc_del_root(GC* gc, void** root) -> void;
+
+  /**
+   * @brief Enumeration of events that can be notified to the garbage collector.
+   *
+   * These events are used to inform the garbage collector about significant
+   * occurrences in the system, such as task management or object finalization.
+   * The GC requires these notifications to function correctly, as it may
+   * need to adjust its behavior based on the current state of tasks and objects.
+   *
+   * The values of this enumeration are used as parameters in the azide_gc_notify function.
+   */
+  enum class Event : uint8_t {
+    /**
+     * @brief New coroutine/task has been created.
+     *
+     * This event must be notified after the task id becomes valid,
+     * but before the task starts running. This is typically done in
+     * the task's constructor or initialization phase.
+     *
+     * @param p The task id of the newly created task. This id is used as an opaque handle
+     *          to refer to the task in subsequent notifications. The handle need not be
+     *          a OS thread id, or any particular type, but must be unique
+     *          within the context of the GC instance.
+     */
+    TaskCreated = 0,
+
+    /**
+     * @brief Task exiting event.
+     *
+     * This event must be notified right before the task terminates.
+     * This task_id must still be valid, and must match the one used
+     * when the task was created with TaskCreated.
+     *
+     * @param p The task id of the task that is exiting.
+     */
+    TaskExited = 1,
+
+    /**
+     * @brief Task blocked event.
+     *
+     * This event must be notified when a task is blocked, meaning it is waiting
+     * for a resource or condition to become available before it can continue execution.
+     * If the `TaskExited` has been received for the task_id, this function is a no-op.
+     * If the task is already blocked, this function is a no-op as well.
+     *
+     * @param p The task id of the task that is blocked. This id must match the one used
+     *          when the task was created with TaskCreated.
+     */
+    TaskBlocked = 2,
+
+    /**
+     * @brief Task unblocked event.
+     *
+     * This event must be notified when a previously blocked task is now able to continue execution,
+     * meaning the resource or condition it was waiting for has become available.
+     * If the `TaskExited` has been received for the task_id, this function is a no-op.
+     * If the task is not blocked, this function is a no-op as well.
+     *
+     * @param p The task id of the task that is unblocked. This id must match the one used
+     *          when the task was created with TaskCreated.
+     */
+    TaskUnblocked = 3,
+
+    /**
+     * @brief Asynchronous finalization event.
+     *
+     * This event must be notified when an object is finalized and is therefore
+     * able to be deallocated.
+     *
+     * @param p The object id of the finalized object. This is the same id provided to
+     * the m_runner callback's `object_id` parameter when the GC was created.
+     */
+    ObjectDestructed = 10,
+  };
+
+  /**
+   * @brief Notifies the garbage collector of an event.
+   *
+   * This function is used to inform the garbage collector about specific events
+   * that may affect its operation, such as task management or object finalization.
+   *
+   * @param gc   Pointer to the GC instance.
+   * @param event The event type to notify the GC about.
+   * @param x    An additional parameter that may be used to provide context for the event.
+   *             The meaning of this parameter depends on the event type.
+   * @return true if the notification was successfully processed, false otherwise.
+   *
+   * @note This function is **thread-safe**, including with respect to the GC instance.
+   */
+  extern "C" [[nodiscard]] auto azide_gc_notify(GC* gc, Event event, uint64_t p) -> bool;
 
   extern "C" auto azide_gc_step(GC* gc) -> uint64_t;
+  extern "C" auto azide_gc_catchup(GC* gc) -> uint64_t;
 
   extern "C" auto azide_gc_malloc(GC* gc, size_t size, size_t align) -> void*;
-  extern "C" auto azide_gc_sizeof(GC* gc, const void* ptr) -> size_t;
 }  // namespace azide::gc
 
 #endif
-
-static void test() {
-  using namespace azide::gc;
-
-  auto* gc = azide_gc_create(
-      {
-          .m_suspend = nullptr, /* Suspend callback */
-          .m_resume = nullptr,  /* Resume callback */
-      },
-      {
-          .m_runner = nullptr, /* Async finalization callback */
-      });
-  if (gc == nullptr) {
-    // Handle error: GC creation failed
-    return;
-  }
-
-  const auto heap_size = 1024 * 1024;  // 1 MB heap
-  std::array<uint8_t, heap_size> heap;
-
-  azide_gc_manage(gc, heap.data(), heap.size());
-  azide_gc_add_task(gc, 1);
-  azide_gc_add_task(gc, 2);
-  azide_gc_add_task(gc, 3);
-  azide_gc_enable(gc);
-
-  azide_gc_step(gc);
-
-  azide_gc_destroy(gc);
-}
