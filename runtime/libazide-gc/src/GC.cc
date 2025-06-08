bbab9c1acd2e817b177b75da36d33ababd512ed8
Wesley Jones <wesjones2004@gmail.com>
2025-06-08 14:04:49 -0400

    azide-gc: work

--- runtime/libazide-gc/src/GC.cc
+++ runtime/libazide-gc/src/GC.cc
@@ -129,6 +129,8 @@ namespace azide::gc {
   }
 
   auto azide_gc_manage(GC* gc, void* base, size_t size) -> bool {
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Manage the memory range
     (void)gc;
     (void)base;
@@ -145,6 +147,10 @@ namespace azide::gc {
   }
 
   auto azide_gc_is_managed(GC* gc, void* base, size_t size) -> bool {
+    assert(gc != nullptr && "GC instance must not be null");
+
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Check if the memory range is managed
     (void)gc;
     (void)base;
@@ -154,6 +160,8 @@ namespace azide::gc {
   }
 
   auto azide_gc_add_root(GC* gc, void** root) -> bool {
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Add a root pointer
     (void)gc;
     (void)root;
@@ -162,27 +170,62 @@ namespace azide::gc {
   }
 
   auto azide_gc_del_root(GC* gc, void** root) -> void {
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Remove a root pointer
     (void)gc;
     (void)root;
   }
 
   auto azide_gc_notify(GC* gc, Event event, uint64_t p) -> bool {
-    // TODO: Handle the event notification
-    (void)gc;
-    (void)event;
+    assert(gc != nullptr && "GC instance must not be null");
+
     (void)p;
 
-    return false;
+    switch (event) {
+      case Event::TaskCreated: {
+        // TODO: Handle task creation event
+        return false;
+      }
+
+      case Event::TaskExited: {
+        // TODO: Handle task exit event
+        return false;
+      }
+
+      case Event::TaskBlocked: {
+        // TODO: Handle task blocked event
+        return false;
+      }
+
+      case Event::TaskUnblocked: {
+        // TODO: Handle task unblocked event
+        return false;
+      }
+
+      case Event::ObjectDestructed: {
+        // TODO: Handle object destructed event
+        return false;
+      }
+
+      default: {
+        // Unknown event type, unhandlable
+        return false;
+      }
+    }
   }
 
   auto azide_gc_step(GC* gc) -> uint64_t {
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Perform a deterministic unit of work for the GC
     (void)gc;
     return 0;
   }
 
   auto azide_gc_catchup(GC* gc) -> uint64_t {
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Perform all pending work for the GC
     (void)gc;
 
@@ -190,6 +233,8 @@ namespace azide::gc {
   }
 
   auto azide_gc_malloc(GC* gc, size_t size, size_t align) -> void* {
+    assert(gc != nullptr && "GC instance must not be null");
+
     // TODO: Allocate memory with the GC
     (void)gc;
     (void)size;
