b01eb75271f830de17a71c3b321db8213932245d
Wesley Jones <wesjones2004@gmail.com>
2025-07-14 15:44:22 -0400

    Lexer development (#58)
    * rm
    
    * move
    
    * move
    
    * move
    
    * move
    
    * work
    
    * work
    
    * rm
    
    * rm
    
    * work
    
    * work
    
    * work
    
    * work
    
    * build test binary
    
    * work
    
    * work
    
    * Support comments
    
    * Typical and Atypical identifiers
    
    * Keyword detection
    
    * work
    
    * Punctuation lexing support
    
    * Operator lexing support
    
    * Work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * fix issues
    
    * fix issues
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * rm
    
    * work
    
    * work
    
    * work
    
    * optimize
    
    * optimize
    
    * work
    
    * optimize
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * restructure project
    
    * restructure project

---
+++ src/bin/nitc.rs
@@ -0,0 +1,45 @@
+use nitrate_compiler::lexer::*;
+use std::io::Read;
+
+fn main() {
+    env_logger::Builder::from_default_env()
+        .format_timestamp(None)
+        .format_level(false)
+        .format_target(false)
+        .init();
+
+    let filename = "stdin";
+    let mut source_code = Vec::new();
+
+    if let Err(e) = std::io::stdin().read_to_end(&mut source_code) {
+        eprintln!("Error reading from stdin: {}", e);
+        return;
+    }
+
+    let mut storage = StringStorage::new();
+
+    let lexer = Lexer::new(&source_code, filename, &mut storage);
+    if lexer.is_err() {
+        eprintln!("Failed to create lexer");
+        return;
+    }
+
+    let mut lexer = lexer.unwrap();
+
+    loop {
+        let token = lexer.next_token();
+        match token.token() {
+            Token::Eof => {
+                println!("End of file reached.");
+                break;
+            }
+            Token::Illegal => {
+                eprintln!("Illegal token encountered: {:?}", token);
+                break;
+            }
+            _ => {
+                println!("{:?}", token);
+            }
+        }
+    }
+}
