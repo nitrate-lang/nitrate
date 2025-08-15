20798d613893188eb805916acb6c7a57346caf96
Wesley Jones <wesjones2004@gmail.com>
2025-08-15 00:03:54 -0400

    work

--- compiler/src/evaluate/abstract_machine.rs
+++ compiler/src/evaluate/abstract_machine.rs
@@ -1,18 +1,18 @@
-use crate::parsetree::{Builder, ExprKey, Storage, node::Variable};
+use crate::parsetree::{Builder, ExprKey, ExprRef, Storage, TypeKey, TypeRef, node::Variable};
 use hashbrown::HashMap;
 
 #[derive(Debug, Default)]
 struct CallFrame<'a> {
-    variables_and_parameters: HashMap<&'a str, Variable<'a>>,
+    function_locals: HashMap<&'a str, Variable<'a>>,
 }
 
 impl<'a> CallFrame<'a> {
     fn get(&self, name: &str) -> Option<&Variable<'a>> {
-        self.variables_and_parameters.get(name)
+        self.function_locals.get(name)
     }
 
     fn set(&mut self, name: &'a str, variable: Variable<'a>) {
-        self.variables_and_parameters.insert(name, variable);
+        self.function_locals.insert(name, variable);
     }
 }
 
@@ -23,7 +23,7 @@ struct Task<'a> {
     task_id: u64,
 }
 
-impl<'a, 'storage> Task<'a> {
+impl<'a> Task<'a> {
     fn new(task_id: u64) -> Self {
         let call_stack = Vec::from([CallFrame::default()]);
         Task {
@@ -42,11 +42,11 @@ pub enum FunctionError {
 pub type Function<'a, 'storage> =
     fn(&mut AbstractMachine<'a, 'storage>) -> Result<ExprKey<'a>, FunctionError>;
 
-#[derive(Debug)]
-pub struct AbstractMachine<'a, 'storage> {
+pub struct AbstractMachine<'a, 'storage>
+where
+    'a: 'storage,
+{
     storage: &'storage mut Storage<'a>,
-    program: ExprKey<'a>,
-
     global_variables: HashMap<&'a str, Variable<'a>>,
     provided_functions: HashMap<&'a str, Function<'a, 'storage>>,
     tasks: Vec<Task<'a>>,
@@ -54,10 +54,9 @@ pub struct AbstractMachine<'a, 'storage> {
 }
 
 impl<'a, 'storage> AbstractMachine<'a, 'storage> {
-    pub fn new(program: ExprKey<'a>, storage: &'storage mut Storage<'a>) -> Self {
+    pub fn new(s: &'storage mut Storage<'a>) -> Self {
         let mut abstract_machine = AbstractMachine {
-            storage,
-            program,
+            storage: s,
             global_variables: HashMap::new(),
             provided_functions: HashMap::new(),
             tasks: Vec::from([Task::new(0)]),
@@ -70,23 +69,26 @@ impl<'a, 'storage> AbstractMachine<'a, 'storage> {
     }
 
     fn setup_builtins(&mut self) {
-        self.provide_function("print", |m| {
-            let value_to_print = m.tasks[m.current_task]
+        self.provide_function("std::intrinsic::print", |m| {
+            let string_to_print = m.tasks[m.current_task]
                 .call_stack
                 .last()
                 .expect("No call frame found")
-                .get("value")
+                .get("msg")
                 .ok_or(FunctionError::MissingArgument)?
                 .value()
                 .ok_or(FunctionError::MissingArgument)?;
 
-            // FIXME: Lookup the ToString trait implementation for the type of value_to_print
-            println!("{:?}", value_to_print.as_printable(m.storage));
-
-            Ok(Builder::new(m.storage).get_unit().into())
+            match string_to_print.get(m.storage) {
+                ExprRef::StringLit(string) => {
+                    print!("{}", string.get());
+                    Ok(Builder::new(m.storage).get_unit().into())
+                }
+                _ => Err(FunctionError::TypeError),
+            }
         });
 
-        // TODO: Implement default intrinsic functions
+        // TODO: Implement other intrinsic functions
     }
 
     fn setup_runtime(&mut self) {
@@ -102,8 +104,287 @@ impl<'a, 'storage> AbstractMachine<'a, 'storage> {
         self.provided_functions.insert(name, callback)
     }
 
-    pub fn evaluate(&mut self) -> Option<ExprKey<'a>> {
-        // TODO: Interpret the program starting from self.program
-        None
+    pub fn evaluate(&mut self, expression: ExprKey<'a>) -> ExprKey<'a> {
+        match expression.get(self.storage) {
+            ExprRef::Bool
+            | ExprRef::UInt8
+            | ExprRef::UInt16
+            | ExprRef::UInt32
+            | ExprRef::UInt64
+            | ExprRef::UInt128
+            | ExprRef::Int8
+            | ExprRef::Int16
+            | ExprRef::Int32
+            | ExprRef::Int64
+            | ExprRef::Int128
+            | ExprRef::Float8
+            | ExprRef::Float16
+            | ExprRef::Float32
+            | ExprRef::Float64
+            | ExprRef::Float128
+            | ExprRef::InferType
+            | ExprRef::TypeName(_)
+            | ExprRef::OpaqueType(_)
+            | ExprRef::RefinementType(_)
+            | ExprRef::TupleType(_)
+            | ExprRef::ArrayType(_)
+            | ExprRef::MapType(_)
+            | ExprRef::SliceType(_)
+            | ExprRef::FunctionType(_)
+            | ExprRef::ManagedRefType(_)
+            | ExprRef::UnmanagedRefType(_)
+            | ExprRef::GenericType(_) => {
+                let type_expression = expression.try_into().expect("Expected type expression");
+                self.evaluate_type(type_expression).into()
+            }
+
+            ExprRef::Discard => {
+                return Builder::new(self.storage).get_discard();
+            }
+
+            ExprRef::BooleanLit(_)
+            | ExprRef::IntegerLit(_)
+            | ExprRef::FloatLit(_)
+            | ExprRef::StringLit(_)
+            | ExprRef::BStringLit(_) => expression,
+
+            ExprRef::ListLit(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::ObjectLit(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::UnaryExpr(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::BinExpr(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Statement(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Block(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Function(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Variable(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Identifier(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Scope(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::If(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::WhileLoop(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::DoWhileLoop(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Switch(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Break(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Continue(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Return(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::ForEach(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Await(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            ExprRef::Assert(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+        }
+    }
+
+    pub fn evaluate_type(&mut self, type_expression: TypeKey<'a>) -> TypeKey<'a> {
+        match type_expression.get(self.storage) {
+            TypeRef::Bool
+            | TypeRef::UInt8
+            | TypeRef::UInt16
+            | TypeRef::UInt32
+            | TypeRef::UInt64
+            | TypeRef::UInt128
+            | TypeRef::Int8
+            | TypeRef::Int16
+            | TypeRef::Int32
+            | TypeRef::Int64
+            | TypeRef::Int128
+            | TypeRef::Float8
+            | TypeRef::Float16
+            | TypeRef::Float32
+            | TypeRef::Float64
+            | TypeRef::Float128
+            | TypeRef::InferType
+            | TypeRef::TypeName(_)
+            | TypeRef::OpaqueType(_) => type_expression,
+
+            TypeRef::RefinementType(refinement_type) => {
+                let (base_type, width, min, max) = (
+                    refinement_type.base(),
+                    refinement_type.width(),
+                    refinement_type.min(),
+                    refinement_type.max(),
+                );
+
+                let base_type = self.evaluate_type(base_type);
+                let width = width.map(|w| self.evaluate(w));
+                let min = min.map(|m| self.evaluate(m));
+                let max = max.map(|m| self.evaluate(m));
+
+                Builder::new(self.storage)
+                    .create_refinement_type()
+                    .with_base(base_type)
+                    .with_width(width)
+                    .with_minimum(min)
+                    .with_maximum(max)
+                    .build()
+            }
+
+            TypeRef::TupleType(tuple_type) => {
+                let element_types = tuple_type
+                    .elements()
+                    .to_vec()
+                    .iter_mut()
+                    .map(|t| self.evaluate_type(*t))
+                    .collect::<Vec<_>>();
+
+                Builder::new(self.storage)
+                    .create_tuple_type()
+                    .add_elements(element_types)
+                    .build()
+            }
+
+            TypeRef::ArrayType(array_type) => {
+                let (element_type, count) = (array_type.element(), array_type.count());
+                let element_type = self.evaluate_type(element_type);
+                let count = self.evaluate(count);
+
+                Builder::new(self.storage)
+                    .create_array_type()
+                    .with_element(element_type)
+                    .with_count(count)
+                    .build()
+            }
+
+            TypeRef::MapType(map_type) => {
+                let (key_type, value_type) = (map_type.key(), map_type.value());
+                let key_type = self.evaluate_type(key_type);
+                let value_type = self.evaluate_type(value_type);
+
+                Builder::new(self.storage)
+                    .create_map_type()
+                    .with_key(key_type)
+                    .with_value(value_type)
+                    .build()
+            }
+
+            TypeRef::SliceType(slice_type) => {
+                let element_type = self.evaluate_type(slice_type.element());
+
+                Builder::new(self.storage)
+                    .create_slice_type()
+                    .with_element(element_type)
+                    .build()
+            }
+
+            TypeRef::FunctionType(_) => {
+                // TODO: Evaluate variant
+                unimplemented!()
+            }
+
+            TypeRef::ManagedRefType(ref_type) => {
+                let is_mutable = ref_type.is_mutable();
+                let target_type = self.evaluate_type(ref_type.target());
+
+                Builder::new(self.storage)
+                    .create_managed_type()
+                    .with_mutability(is_mutable)
+                    .with_target(target_type)
+                    .build()
+            }
+
+            TypeRef::UnmanagedRefType(ref_type) => {
+                let is_mutable = ref_type.is_mutable();
+                let target_type = self.evaluate_type(ref_type.target());
+
+                Builder::new(self.storage)
+                    .create_unmanaged_type()
+                    .with_mutability(is_mutable)
+                    .with_target(target_type)
+                    .build()
+            }
+
+            TypeRef::GenericType(generic_type) => {
+                let (generic_base, mut generic_args) =
+                    (generic_type.base(), generic_type.arguments().to_vec());
+
+                let generic_args = generic_args
+                    .iter_mut()
+                    .map(|(name, arg)| (*name, self.evaluate(*arg)))
+                    .collect::<Vec<_>>();
+
+                let generic_base = self.evaluate_type(generic_base);
+
+                Builder::new(self.storage)
+                    .create_generic_type()
+                    .with_base(generic_base)
+                    .add_arguments(generic_args)
+                    .build()
+            }
+        }
     }
 }
