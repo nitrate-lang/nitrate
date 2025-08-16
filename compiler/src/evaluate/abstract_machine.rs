00f67948925973b4c9ba959ce68cb21ce0d87497
Wesley Jones <wesjones2004@gmail.com>
2025-08-15 21:38:26 -0400

    work

--- compiler/src/evaluate/abstract_machine.rs
+++ compiler/src/evaluate/abstract_machine.rs
@@ -43,6 +43,10 @@ pub enum FunctionError {
 
 pub type Function<'a> = fn(&mut AbstractMachine<'a>) -> Result<Expr<'a>, FunctionError>;
 
+pub enum EvalError {
+    TypeError,
+}
+
 pub struct AbstractMachine<'a> {
     global_variables: HashMap<&'a str, Variable<'a>>,
     provided_functions: HashMap<&'a str, Function<'a>>,
@@ -103,27 +107,27 @@ impl<'a> AbstractMachine<'a> {
         self.provided_functions.insert(name, callback)
     }
 
-    pub fn evaluate(&mut self, expression: &Expr<'a>) -> Expr<'a> {
+    pub fn evaluate(&mut self, expression: &Expr<'a>) -> Result<Expr<'a>, EvalError> {
         match expression {
-            Expr::Bool => Expr::Bool,
-            Expr::UInt8 => Expr::UInt8,
-            Expr::UInt16 => Expr::UInt16,
-            Expr::UInt32 => Expr::UInt32,
-            Expr::UInt64 => Expr::UInt64,
-            Expr::UInt128 => Expr::UInt128,
-            Expr::Int8 => Expr::Int8,
-            Expr::Int16 => Expr::Int16,
-            Expr::Int32 => Expr::Int32,
-            Expr::Int64 => Expr::Int64,
-            Expr::Int128 => Expr::Int128,
-            Expr::Float8 => Expr::Float8,
-            Expr::Float16 => Expr::Float16,
-            Expr::Float32 => Expr::Float32,
-            Expr::Float64 => Expr::Float64,
-            Expr::Float128 => Expr::Float128,
-            Expr::InferType => Expr::InferType,
-            Expr::TypeName(str) => Expr::TypeName(str),
-            Expr::OpaqueType(identity) => Expr::OpaqueType(identity.clone()),
+            Expr::Bool => Ok(Expr::Bool),
+            Expr::UInt8 => Ok(Expr::UInt8),
+            Expr::UInt16 => Ok(Expr::UInt16),
+            Expr::UInt32 => Ok(Expr::UInt32),
+            Expr::UInt64 => Ok(Expr::UInt64),
+            Expr::UInt128 => Ok(Expr::UInt128),
+            Expr::Int8 => Ok(Expr::Int8),
+            Expr::Int16 => Ok(Expr::Int16),
+            Expr::Int32 => Ok(Expr::Int32),
+            Expr::Int64 => Ok(Expr::Int64),
+            Expr::Int128 => Ok(Expr::Int128),
+            Expr::Float8 => Ok(Expr::Float8),
+            Expr::Float16 => Ok(Expr::Float16),
+            Expr::Float32 => Ok(Expr::Float32),
+            Expr::Float64 => Ok(Expr::Float64),
+            Expr::Float128 => Ok(Expr::Float128),
+            Expr::InferType => Ok(Expr::InferType),
+            Expr::TypeName(str) => Ok(Expr::TypeName(str)),
+            Expr::OpaqueType(identity) => Ok(Expr::OpaqueType(identity.clone())),
 
             Expr::RefinementType(_)
             | Expr::TupleType(_)
@@ -139,36 +143,36 @@ impl<'a> AbstractMachine<'a> {
                     .clone()
                     .try_into()
                     .expect("Expected type expression");
-                self.evaluate_type(&type_expression).into()
+                self.evaluate_type(&type_expression).map(|t| t.into())
             }
 
-            Expr::Discard => Expr::Discard,
+            Expr::Discard => Ok(Expr::Discard),
             Expr::HasParentheses(expr) => self.evaluate(expr.deref()),
 
-            Expr::BooleanLit(bool) => Expr::BooleanLit(bool.to_owned()),
-            Expr::IntegerLit(int) => Expr::IntegerLit(int.clone()),
-            Expr::FloatLit(float) => Expr::FloatLit(float.to_owned()),
-            Expr::StringLit(string) => Expr::StringLit(string.clone()),
-            Expr::BStringLit(bstring) => Expr::BStringLit(bstring.clone()),
+            Expr::BooleanLit(bool) => Ok(Expr::BooleanLit(bool.to_owned())),
+            Expr::IntegerLit(int) => Ok(Expr::IntegerLit(int.clone())),
+            Expr::FloatLit(float) => Ok(Expr::FloatLit(float.to_owned())),
+            Expr::StringLit(string) => Ok(Expr::StringLit(string.clone())),
+            Expr::BStringLit(bstring) => Ok(Expr::BStringLit(bstring.clone())),
 
             Expr::ListLit(list) => {
-                let elements = list
-                    .elements()
-                    .iter()
-                    .map(|e| self.evaluate(e))
-                    .collect::<Vec<_>>();
+                let mut elements = Vec::new();
+                elements.reserve(list.elements().len());
 
-                Builder::create_list().add_elements(elements).build()
+                for element in list.elements() {
+                    elements.push(self.evaluate(element)?);
+                }
+
+                Ok(Builder::create_list().add_elements(elements).build())
             }
 
             Expr::ObjectLit(object) => {
-                let fields = object
-                    .get()
-                    .iter()
-                    .map(|(k, v)| (*k, self.evaluate(v)))
-                    .collect::<BTreeMap<_, _>>();
+                let mut fields = BTreeMap::new();
+                for (key, value) in object.get() {
+                    fields.insert(*key, self.evaluate(value)?);
+                }
 
-                Builder::create_object().add_fields(fields).build()
+                Ok(Builder::create_object().add_fields(fields).build())
             }
 
             Expr::UnaryExpr(_) => {
@@ -182,17 +186,17 @@ impl<'a> AbstractMachine<'a> {
             }
 
             Expr::Statement(statement) => {
-                self.evaluate(&statement.get());
-                Builder::get_unit().into()
+                self.evaluate(&statement.get())?;
+                Ok(Builder::get_unit().into())
             }
 
             Expr::Block(block) => {
                 let mut result = None;
                 for element in block.elements() {
-                    result = Some(self.evaluate(element));
+                    result = Some(self.evaluate(element)?);
                 }
 
-                result.unwrap_or_else(|| Builder::get_unit().into())
+                Ok(result.unwrap_or_else(|| Builder::get_unit().into()))
             }
 
             Expr::Function(_) => {
@@ -215,10 +219,19 @@ impl<'a> AbstractMachine<'a> {
                 unimplemented!()
             }
 
-            Expr::If(_) => {
-                // TODO: Evaluate if
-                unimplemented!()
-            }
+            Expr::If(if_expr) => match self.evaluate(&if_expr.condition())? {
+                Expr::BooleanLit(b) => {
+                    if b {
+                        self.evaluate(&if_expr.then_branch())
+                    } else if let Some(else_branch) = if_expr.else_branch() {
+                        self.evaluate(else_branch)
+                    } else {
+                        Ok(Builder::get_unit().into())
+                    }
+                }
+
+                _ => panic!("Condition did not evaluate to a boolean"),
+            },
 
             Expr::WhileLoop(_) => {
                 // TODO: Evaluate while loop
@@ -267,145 +280,159 @@ impl<'a> AbstractMachine<'a> {
         }
     }
 
-    pub fn evaluate_type(&mut self, type_expression: &Type<'a>) -> Type<'a> {
+    pub fn evaluate_type(&mut self, type_expression: &Type<'a>) -> Result<Type<'a>, EvalError> {
         if self.already_evaluated_types.contains(type_expression) {
-            return type_expression.to_owned();
+            return Ok(type_expression.to_owned());
         }
 
         let result = match type_expression {
-            Type::Bool => Type::Bool,
-            Type::UInt8 => Type::UInt8,
-            Type::UInt16 => Type::UInt16,
-            Type::UInt32 => Type::UInt32,
-            Type::UInt64 => Type::UInt64,
-            Type::UInt128 => Type::UInt128,
-            Type::Int8 => Type::Int8,
-            Type::Int16 => Type::Int16,
-            Type::Int32 => Type::Int32,
-            Type::Int64 => Type::Int64,
-            Type::Int128 => Type::Int128,
-            Type::Float8 => Type::Float8,
-            Type::Float16 => Type::Float16,
-            Type::Float32 => Type::Float32,
-            Type::Float64 => Type::Float64,
-            Type::Float128 => Type::Float128,
-            Type::InferType => Type::InferType,
-            Type::TypeName(name) => Type::TypeName(*name),
-            Type::OpaqueType(identity) => Type::OpaqueType(identity.clone()),
+            Type::Bool => Ok(Type::Bool),
+            Type::UInt8 => Ok(Type::UInt8),
+            Type::UInt16 => Ok(Type::UInt16),
+            Type::UInt32 => Ok(Type::UInt32),
+            Type::UInt64 => Ok(Type::UInt64),
+            Type::UInt128 => Ok(Type::UInt128),
+            Type::Int8 => Ok(Type::Int8),
+            Type::Int16 => Ok(Type::Int16),
+            Type::Int32 => Ok(Type::Int32),
+            Type::Int64 => Ok(Type::Int64),
+            Type::Int128 => Ok(Type::Int128),
+            Type::Float8 => Ok(Type::Float8),
+            Type::Float16 => Ok(Type::Float16),
+            Type::Float32 => Ok(Type::Float32),
+            Type::Float64 => Ok(Type::Float64),
+            Type::Float128 => Ok(Type::Float128),
+            Type::InferType => Ok(Type::InferType),
+            Type::TypeName(name) => Ok(Type::TypeName(*name)),
+            Type::OpaqueType(identity) => Ok(Type::OpaqueType(identity.clone())),
 
             Type::RefinementType(refinement) => {
-                let width = refinement.width().map(|w| self.evaluate(&w));
-                let min = refinement.min().map(|m| self.evaluate(&m));
-                let max = refinement.max().map(|m| self.evaluate(&m));
-                let base = self.evaluate_type(&refinement.base());
+                let width = match refinement.width() {
+                    Some(w) => Some(self.evaluate(&w)?),
+                    None => None,
+                };
 
-                Builder::create_refinement_type()
+                let min = match refinement.min() {
+                    Some(m) => Some(self.evaluate(&m)?),
+                    None => None,
+                };
+
+                let max = match refinement.max() {
+                    Some(m) => Some(self.evaluate(&m)?),
+                    None => None,
+                };
+
+                let base = self.evaluate_type(&refinement.base())?;
+
+                Ok(Builder::create_refinement_type()
                     .with_width(width)
                     .with_minimum(min)
                     .with_maximum(max)
                     .with_base(base)
-                    .build()
+                    .build())
             }
 
             Type::TupleType(tuple) => {
-                let elements = tuple
-                    .elements()
-                    .iter()
-                    .map(|t| self.evaluate_type(t))
-                    .collect::<Vec<_>>();
+                let mut elements = Vec::new();
+                elements.reserve(tuple.elements().len());
 
-                Builder::create_tuple_type().add_elements(elements).build()
+                for element in tuple.elements() {
+                    elements.push(self.evaluate_type(element)?);
+                }
+
+                Ok(Builder::create_tuple_type().add_elements(elements).build())
             }
 
             Type::ArrayType(array) => {
-                let element = self.evaluate_type(&array.element());
-                let count = self.evaluate(&array.count());
+                let element = self.evaluate_type(&array.element())?;
+                let count = self.evaluate(&array.count())?;
 
-                Builder::create_array_type()
+                Ok(Builder::create_array_type()
                     .with_element(element)
                     .with_count(count)
-                    .build()
+                    .build())
             }
 
             Type::MapType(map) => {
-                let key = self.evaluate_type(&map.key());
-                let value = self.evaluate_type(&map.value());
+                let key = self.evaluate_type(&map.key())?;
+                let value = self.evaluate_type(&map.value())?;
 
-                Builder::create_map_type()
+                Ok(Builder::create_map_type()
                     .with_key(key)
                     .with_value(value)
-                    .build()
+                    .build())
             }
 
             Type::SliceType(slice) => {
-                let element = self.evaluate_type(&slice.element());
-                Builder::create_slice_type().with_element(element).build()
+                let element = self.evaluate_type(&slice.element())?;
+                Ok(Builder::create_slice_type().with_element(element).build())
             }
 
             Type::FunctionType(function) => {
                 let attributes = function.attributes().to_vec();
 
-                let parameters = function
-                    .parameters()
-                    .iter()
-                    .map(|p| {
-                        let type_ = self.evaluate_type(&p.type_());
-                        let default = p.default();
-                        FunctionParameter::new(p.name(), type_, default)
-                    })
-                    .collect::<Vec<_>>();
+                let mut parameters = Vec::new();
+                parameters.reserve(function.parameters().len());
 
-                let return_type = self.evaluate_type(&function.return_type());
+                for parameter in function.parameters() {
+                    let type_ = self.evaluate_type(&parameter.type_())?;
+                    let default = parameter.default();
+                    parameters.push(FunctionParameter::new(parameter.name(), type_, default));
+                }
 
-                Builder::create_function_type()
+                let return_type = self.evaluate_type(&function.return_type())?;
+
+                Ok(Builder::create_function_type()
                     .add_attributes(attributes)
                     .add_parameters(parameters)
                     .with_return_type(return_type)
-                    .build()
+                    .build())
             }
 
             Type::ManagedRefType(reference) => {
                 let is_mutable = reference.is_mutable();
-                let target_type = self.evaluate_type(&reference.target());
+                let target_type = self.evaluate_type(&reference.target())?;
 
-                Builder::create_managed_type()
+                Ok(Builder::create_managed_type()
                     .with_mutability(is_mutable)
                     .with_target(target_type)
-                    .build()
+                    .build())
             }
 
             Type::UnmanagedRefType(reference) => {
                 let is_mutable = reference.is_mutable();
-                let target_type = self.evaluate_type(&reference.target());
+                let target_type = self.evaluate_type(&reference.target())?;
 
-                Builder::create_unmanaged_type()
+                Ok(Builder::create_unmanaged_type()
                     .with_mutability(is_mutable)
                     .with_target(target_type)
-                    .build()
+                    .build())
             }
 
             Type::GenericType(generic) => {
                 // TODO: Instantiate generic base with type arguments
 
-                let arguments = generic
-                    .arguments()
-                    .iter()
-                    .map(|(name, type_)| (*name, self.evaluate(type_)))
-                    .collect::<Vec<_>>();
+                let mut arguments = Vec::new();
+                arguments.reserve(generic.arguments().len());
 
-                let base = self.evaluate_type(&generic.base());
+                for (name, value) in generic.arguments() {
+                    let evaluated_type = self.evaluate(value)?;
+                    arguments.push((*name, evaluated_type));
+                }
 
-                Builder::create_generic_type()
+                let base = self.evaluate_type(&generic.base())?;
+
+                Ok(Builder::create_generic_type()
                     .add_arguments(arguments)
                     .with_base(base)
-                    .build()
+                    .build())
             }
 
             Type::HasParenthesesType(inner) => self.evaluate_type(inner),
         };
 
-        self.already_evaluated_types.insert(result.clone());
-
-        result
+        result.inspect(|t| {
+            self.already_evaluated_types.insert(t.clone());
+        })
     }
 }
