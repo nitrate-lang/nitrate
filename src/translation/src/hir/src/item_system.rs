90585b3ae56425ea3b79eea99df671819940a604
Wesley Jones <wesjones2004@gmail.com>
2025-09-12 18:32:02 -0400

    work

--- src/translation/src/hir/src/item_system.rs
+++ src/translation/src/hir/src/item_system.rs
@@ -1,3 +1,26 @@
+use crate::ItemStore;
+
 use serde::{Deserialize, Serialize};
 
-pub type Item = ();
+#[derive(Serialize, Deserialize)]
+pub enum Item {}
+
+impl Item {}
+
+impl Item {
+    pub fn dump(
+        &self,
+        _storage: &ItemStore,
+        o: &mut dyn std::fmt::Write,
+    ) -> Result<(), std::fmt::Error> {
+        match self {
+            _ => write!(o, "<unimplemented>"),
+        }
+    }
+
+    pub fn dump_string(&self, storage: &ItemStore) -> String {
+        let mut buf = String::new();
+        self.dump(storage, &mut buf).ok();
+        buf
+    }
+}
