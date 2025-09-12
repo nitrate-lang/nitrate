b7d09f4c28ddf078a9811a27e03018a2298aede8
Wesley Jones <wesjones2004@gmail.com>
2025-09-12 18:30:18 -0400

    work

---
+++ src/translation/src/hir/src/item_store.rs
@@ -0,0 +1,52 @@
+use crate::item_system::Item;
+
+use hashbrown::HashMap;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct ItemId {
+    id: u32,
+}
+
+#[derive(Default)]
+pub struct ItemStore {
+    items: HashMap<ItemId, Item>,
+    next_id: u32,
+}
+
+impl ItemStore {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn store(&mut self, item: Item) -> ItemId {
+        let id = self.next_id;
+        self.items.insert(ItemId { id }, item);
+        self.next_id += 1;
+        ItemId { id }
+    }
+
+    fn get(&self, id: &ItemId) -> &Item {
+        self.items.get(id).expect("ItemId not found in ItemStore")
+    }
+
+    fn get_mut(&mut self, id: &ItemId) -> &mut Item {
+        self.items
+            .get_mut(id)
+            .expect("ItemId not found in ItemStore")
+    }
+}
+
+impl std::ops::Index<&ItemId> for ItemStore {
+    type Output = Item;
+
+    fn index(&self, index: &ItemId) -> &Self::Output {
+        self.get(index)
+    }
+}
+
+impl std::ops::IndexMut<&ItemId> for ItemStore {
+    fn index_mut(&mut self, index: &ItemId) -> &mut Self::Output {
+        self.get_mut(index)
+    }
+}
--- src/translation/src/hir/src/lib.rs
+++ src/translation/src/hir/src/lib.rs
@@ -1,14 +1,18 @@
 #![warn(clippy::pedantic)]
 
 mod expression;
+mod item_store;
+pub mod item_system;
 mod node_digest;
 mod type_alignment;
 mod type_size;
 mod type_store;
+pub mod type_system;
 
 pub use node_digest::NodeDigest;
 
-pub mod type_system;
+pub use item_store::{ItemId, ItemStore};
+
 pub use type_alignment::get_align_of;
 pub use type_size::get_size_of;
 pub use type_store::{TypeId, TypeStore};
