422f3504b5e09ff7282ed68c25e1bedeaec20619
Wesley Jones <wesjones2004@gmail.com>
2025-10-28 09:07:12 -0400

    work

--- src/translation/src/hir_from_tree/src/put_defaults.rs
+++ src/translation/src/hir_from_tree/src/put_defaults.rs
@@ -1,6 +1,7 @@
 use crate::Ast2HirCtx;
 use nitrate_diagnosis::CompilerLog;
 use nitrate_hir::prelude::*;
+use nitrate_hir_get_type::{TypeInferenceCtx, get_type};
 use std::collections::HashMap;
 
 pub(crate) fn module_put_defaults(module: &mut Module, ctx: &mut Ast2HirCtx, _log: &CompilerLog) {
@@ -45,12 +46,45 @@ pub(crate) fn module_put_defaults(module: &mut Module, ctx: &mut Ast2HirCtx, _lo
             },
 
             Value::MethodCall {
-                object: _,
-                method_name: _,
-                positional: _,
-                named: _,
+                object,
+                method_name,
+                positional,
+                named,
             } => {
-                // TODO: Place default values for arguments
+                let object = &ctx.store[object as &ValueId].borrow();
+                let tyctx = TypeInferenceCtx {
+                    store: &ctx.store,
+                    symbol_tab: &ctx.symbol_tab,
+                };
+
+                if let Ok(object_type) = get_type(object, &tyctx).map(|t| t.into_id(&ctx.store)) {
+                    if let Some(method) = ctx.symbol_tab.get_method(&object_type, method_name) {
+                        let function = &ctx.store[method].borrow();
+
+                        let mut augmented = Vec::new();
+                        augmented.extend(positional.iter().cloned());
+
+                        let mut callee_map = HashMap::new();
+                        for (name, value) in named.iter() {
+                            callee_map.insert(name.clone(), value.clone());
+                        }
+
+                        for signature_param in function.params.iter().skip(positional.len()) {
+                            let signature_param = ctx[signature_param].borrow();
+
+                            if let Some(arg_value) = callee_map.get(&signature_param.name) {
+                                augmented.push(arg_value.clone());
+                            } else if let Some(default_value) = &signature_param.default_value {
+                                augmented.push(default_value.clone());
+                            } else {
+                                // Missing argument without default value
+                            }
+                        }
+
+                        *positional = augmented.into();
+                        named.clear();
+                    }
+                }
             }
 
             _ => {}
