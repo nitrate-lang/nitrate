1b05f3cc3c73f452a6b4103ee74a1653fb4a47dc
Wesley Jones <wesjones2004@gmail.com>
2025-11-25 11:52:12 -0500

    work

--- src/translation/src/tree2/src/lib.rs
+++ src/translation/src/tree2/src/lib.rs
@@ -2,10 +2,16 @@ mod expr;
 mod item;
 mod pat;
 mod stmt;
+mod store;
 mod ty;
 
 pub use expr::*;
 pub use item::*;
 pub use pat::*;
 pub use stmt::*;
+pub use store::*;
 pub use ty::*;
+
+pub mod prelude {
+    pub use super::*;
+}
