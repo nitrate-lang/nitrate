db820d7cd4b8f0a52b86644b5e48d7e67ddd8bd1
Wesley Jones <wesjones2004@gmail.com>
2026-08-03 02:04:44 -0400

    Add nitrate_generate module with initial setup and dependencies

---
+++ src/bin/nitrate_gen.rs
@@ -0,0 +1,48 @@
+use nitrate_driver::Interpreter;
+use slog::{Drain, Record, o};
+use slog_term::{FullFormat, RecordDecorator, TermDecorator, ThreadSafeTimestampFn};
+use std::{process::ExitCode, todo};
+
+fn custom_print_msg_header(
+    _fn_timestamp: &dyn ThreadSafeTimestampFn<Output = std::io::Result<()>>,
+    rd: &mut dyn RecordDecorator,
+    record: &Record,
+    _use_file_location: bool,
+) -> std::io::Result<bool> {
+    rd.start_level()?;
+    write!(rd, "{}", record.level())?;
+
+    rd.start_separator()?;
+    write!(rd, " ")?;
+
+    rd.start_msg()?;
+    write!(rd, "{}", record.msg())?;
+    Ok(true)
+}
+
+#[tokio::main]
+async fn main() -> ExitCode {
+    env_logger::init();
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let decorator = TermDecorator::new().build();
+    let drain = FullFormat::new(decorator)
+        .use_custom_header_print(custom_print_msg_header)
+        .build()
+        .fuse();
+    let drain = slog_async::Async::new(drain)
+        .build()
+        .filter_level(match log::max_level() {
+            log::LevelFilter::Error => slog::Level::Error,
+            log::LevelFilter::Warn => slog::Level::Warning,
+            log::LevelFilter::Info => slog::Level::Info,
+            log::LevelFilter::Debug => slog::Level::Debug,
+            log::LevelFilter::Trace => slog::Level::Trace,
+            log::LevelFilter::Off => slog::Level::Critical,
+        })
+        .fuse();
+    let log = slog::Logger::root(drain, o!());
+
+    todo!("")
+}
